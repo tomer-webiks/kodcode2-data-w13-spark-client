727affb14263b64392fce75d164c873a3f816e9d
Tomer Sagi <tomer_sagi@webiks.com>
2024-12-04 01:11:48 +0200

    Latest version

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from repository.hdfs_repository import create_directory, upload_file
 from services.spark_service import spark_service
+from pyspark.sql.functions import col, avg
 import os
 
 
@@ -15,16 +16,49 @@ def start():
     upload_file(f'{hdfs_folder}/{hdfs_file}', './data/Reviews-250.csv')
 
     # 3. Read the file locally (workaround because of the HDFS issue)
-    file_path = os.path.join(os.getcwd(), "data", "Reviews-250.csv")
-    # df = spark_service.read.csv(f"file:///{file_path}", header=True, inferSchema=True)
-    df = spark_service.read.csv(f"hdfs://namenode:8020/{hdfs_folder}/{hdfs_file}", header=True, inferSchema=True)
+    df = spark_service.read.csv(f"hdfs://192.168.1.100:8020/{hdfs_folder}/{hdfs_file}", header=True, inferSchema=True)
 
-    # Perform a simple transformation
-    df = df.withColumn("grade", df["score"] >= 90)
+    # 1 - Show top 5
+    df.show(5)
+    obj = df.collect()
 
-    # Save the transformed data back to HDFS in Parquet format
-    output_path = "hdfs://localhost:9000/user/hdfs/transformed_data"
-    df.write.parquet(output_path, mode="overwrite")
+
+    # 2 - Filter reviews with a Score of 5
+    high_rating_df = df.filter(df["Score"] == 5)
+    high_rating_df.show()
+
+    # 3 - Select specific columns
+    selected_columns_df = df.select("ProductId", "Score", "Summary")
+    selected_columns_df.show(5)
+    print(selected_columns_df.collect()[0].asDict())
+
+    # 4 - Add HelpfulnessRatio column
+    df = df.withColumn(
+        "HelpfulnessRatio",
+        (col("HelpfulnessNumerator") / col("HelpfulnessDenominator")).cast("double")
+    )
+    df.show(5)
+    
+    # 5 - Filter rows with HelpfulnessDenominator > 0
+    valid_helpfulness_df = df.filter(df["HelpfulnessDenominator"] > 0)
+    valid_helpfulness_df.show(5)
+
+    # 6 - Count distinct UserId values
+    distinct_users_count = df.select("UserId").distinct().count()
+    print(f"Distinct Users: {distinct_users_count}")
+
+    # 20 - Calculate average Score per ProductId
+    average_score_df = df.groupBy("ProductId").agg(avg("Score").alias("AverageScore"))
+    average_score_df.show(5)
+
+
+    # ---- SQL ----
+    df.createOrReplaceTempView("reviews")
+
+    # 1 - Register the DataFrame as a temporary view
+    sql_query = "SELECT * FROM reviews"
+    result = spark_service.sql(sql_query)
+    result.show()
 
 
 if __name__ == '__main__':
--- services/spark_service.py
+++ services/spark_service.py
@@ -6,8 +6,8 @@ def get_spark_session():
     return SparkSession.builder \
         .appName("HDFS-Spark") \
         .config("spark.hadoop.dfs.client.rpc.max-size", "134217728") \
-        .config("spark.hadoop.fs.defaultFS", "hdfs://namenode:8020") \
-        .master("spark://localhost:7077") \
+        .config("spark.hadoop.fs.defaultFS", "hdfs://192.168.1.100:8020") \
+        .master("spark://192.168.1.103:7077") \
         .getOrCreate()
 
 
