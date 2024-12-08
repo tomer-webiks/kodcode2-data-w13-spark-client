f146fe7383dc3c93e509c99c98699cf8923254c9
Tomer Sagi <tomer_sagi@webiks.com>
2024-12-08 09:20:50 +0200

    Fixed incorrect reference

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from tests.test_stream_csv_to_append_json import test_stream_csv_to_append_json
-from tests.test_stream_csv_to_complete_json import test_stream_csv_to_complete_json
 from tests.test_spark_hdfs_df_2 import test_spark_hdfs_df
 import os
 
