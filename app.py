1521275120e7dbd76df1636eaeaaefc3f42c0b4e
Tomer Sagi <tomer_sagi@webiks.com>
2024-12-09 12:11:00 +0200

    Fixed problem. Incorrect py4j and pyspark versions. reverted back to py4j==0.10.9.7, pyspark==3.5.3 and recreated the venv folder

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from tests.test_stream_csv_to_append_json import test_stream_csv_to_append_json
-from tests.test_spark_hdfs_df_2 import test_spark_hdfs_df
+# from tests.test_stream_csv_to_append_json import test_stream_csv_to_append_json
+from tests.test_spark_hdfs_df_2 import test_spark_hdfs_df as test2
+from tests.test_spark_hdfs_df_1 import test_spark_hdfs_df as test1
 import os
 
 if __name__ == '__main__':
-    test_spark_hdfs_df()
+    test1()
+    # test2()
     # test_stream_csv_to_append_json()
     # test_stream_csv_to_complete_json()
